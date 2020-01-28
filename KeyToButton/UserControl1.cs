853dc9d6fe8702f078704623d175267a84376ad0
Bao Phung <phungb@oregonstate.edu>
2020-01-28 13:02:41 -0800

    Add files via upload

---
+++ KeyToButton/UserControl1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyToButton
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World.  This key was pressed: " + e.ToString());
+        }
+
+        private void button1_KeyDown(object sender, KeyPressEventArgs e)
+        {
+            if ( e.KeyChar > 0)
+           
+                button1_Click(this, new EventArgs());    
+        }
+    }
+}
